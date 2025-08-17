e42f0ac738bd17875d1b432533375db5904ca30e
JosephVentrella <ventrellajoe1@gmail.com>
2025-08-17 09:11:02 -0700

    Update player scene and script Update player to have a real character model with a walk animation and add some simple movement and shooting behavior as well as simple unit collision on the player

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,15 +3,23 @@ extends CharacterBody2D
 @export var speed: float = 1000.0
 @export var bullet_scene: PackedScene
 @export var fire_rate: float = 0.15
+var screen_size # Size of the game window.
 var fire_timer: float = 0.0
+
 func _ready():
 	print("Player script loaded")
+	screen_size = get_viewport_rect().size
+
 func _physics_process(_delta):
 	var input_dir = Vector2.ZERO
 	input_dir.x = Input.get_axis("ui_left", "ui_right")
 	input_dir.y = Input.get_axis("ui_up", "ui_down")
 
 	velocity = input_dir.normalized() * speed
+	if velocity.length() > 0:
+		$Sprite2D.play()
+	else:
+		$Sprite2D.stop()
 
 	move_and_slide()
 	
